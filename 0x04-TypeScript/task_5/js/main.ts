d099b7c440035117fd076080d646b67d0d88f600
Jefter83 <jefteradinkrah82@gmail.com>
2025-11-12 15:56:53 -0800

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,1 +1,32 @@
+interface MajorCredits {
+  credits: number;
+  // Unique brand to distinguish MajorCredits
+  brand: 'major';
+}
+
+interface MinorCredits {
+  credits: number;
+  // Unique brand to distinguish MinorCredits
+  brand: 'minor';
+}
+
+function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'major',
+  };
+}
+
+function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'minor',
+  };
+}
 
